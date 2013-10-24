7ade8404fed5c723ec98b16d16a65e3c08b92cc2
David Rubin <david@fireid.com>
2013-10-24 06:33:42 +0200

    back to local host socket

--- templates/scripts/libs/websocket.js
+++ templates/scripts/libs/websocket.js
@@ -1,6 +1,6 @@
 var serverOverride = null;
 // Set up the Alchemy client object
-var serverOverride = "10.20.0.148"; // David vm
+//var serverOverride = "10.20.0.148"; // David vm
 //var serverOverride = "10.10.10.64"; // Admo-room
 //Set this value to make the framework connect to another IP address
 
